c5c948c9e575b4f83884dfef1a233a821fe72d17
Ebube <dbupanyiam@gmail.com>
2024-08-15 16:30:35 +0100

    Update logout

--- src/app/api/auth/admin/logout/route.ts
+++ src/app/api/auth/admin/logout/route.ts
@@ -4,28 +4,32 @@ import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   try {
-    const cookieHeader = request.headers.get("cookie") || "";
+    // const cookieHeader = request.headers.get("cookie") || "";
+
+    console.log("here");
 
     await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/logout`, {
       headers: {
-        Cookie: cookieHeader, // Pass the cookies received from the client
+        Authorization: `Bearer ${cookies().get("auth")?.value}`,
       },
       // withCredentials: true,
     });
 
-    cookies().delete("session");
-    cookies().set("session", "", {
-      httpOnly: true,
-      ...(process.env.NODE_ENV === "production" && {
-        sameSite: "none",
-        secure: true,
-        domain: ".prunepayments.net",
-        // domain: "prune-liard.vercel.app",
-      }),
-    });
+    // cookies().delete("session");
+    // cookies().set("session", "", {
+    //   httpOnly: true,
+    //   ...(process.env.NODE_ENV === "production" && {
+    //     sameSite: "none",
+    //     secure: true,
+    //     domain: ".prunepayments.net",
+    //     // domain: "prune-liard.vercel.app",
+    //   }),
+    // });
 
     return NextResponse.json({});
   } catch (error) {
+    console.log(error);
+
     if (axios.isAxiosError(error)) {
       const res = error.response?.data;
       return NextResponse.json({ message: res.message }, { status: res.code });

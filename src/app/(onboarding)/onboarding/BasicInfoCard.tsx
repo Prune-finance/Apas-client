c38d323de6c52ec5af18991359f4d44aa42da0f1
Gilead Odo <odogilead@gmail.com>
2025-05-13 23:15:20 +0100

    refactor(onboarding): simplify data handling in info cards
    Extract form values directly within BasicInfoCard and CEOInfoCard components to avoid redundant data mapping in ReviewInfo. This improves maintainability and reduces code duplication.

--- src/app/(onboarding)/onboarding/BasicInfoCard.tsx
+++ src/app/(onboarding)/onboarding/BasicInfoCard.tsx
@@ -1,14 +1,41 @@
+import { OnboardingType } from "@/lib/schema";
 import { PrimaryBtn } from "@/ui/components/Buttons";
 import { Box, Flex, Group, Stack, Text } from "@mantine/core";
+import { UseFormReturnType } from "@mantine/form";
 import { IconEdit } from "@tabler/icons-react";
 import React from "react";
 
 interface BasicInfoCard {
   setActive: React.Dispatch<React.SetStateAction<number>>;
-  data: Record<string, string | null | undefined>;
+  form: UseFormReturnType<OnboardingType>;
 }
 
-function BasicInfoCard({ setActive, data }: BasicInfoCard) {
+function BasicInfoCard({ setActive, form }: BasicInfoCard) {
+  const {
+    businessName,
+    businessType,
+    businessAddress,
+    businessCountry,
+    businessDescription,
+    businessEmail,
+    businessIndustry,
+    businessPhoneNumber,
+    businessWebsite,
+    businessTradingName,
+  } = form.values;
+
+  const basicInfo = {
+    "Business Name": businessName,
+    "Trading Name": businessTradingName,
+    "Phone number": businessPhoneNumber,
+    Email: businessEmail,
+    "Business Type": businessType,
+    "Business Industry": businessIndustry,
+    "Business Website (URL)": businessWebsite,
+    Country: businessCountry,
+    "Business Address": businessAddress,
+    "Business Bio": businessDescription,
+  };
   return (
     <Box p={24} bg="#F2F4F7" mt={24} style={{ borderRadius: 8 }}>
       <Flex align="center" justify="space-between" w="100%">
@@ -26,7 +53,7 @@ function BasicInfoCard({ setActive, data }: BasicInfoCard) {
         />
       </Flex>
       <Stack gap={10} mt={20}>
-        {Object.entries(data).map(([key, value]) => (
+        {Object.entries(basicInfo).map(([key, value]) => (
           <Group key={key} justify="space-between" align="start">
             <Text c="#667085" fz={12}>
               {key}:

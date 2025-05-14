c7fda2df7d1878737fcd60c7063eea9e310dab79
Gilead Odo <63629666+blur94@users.noreply.github.com>
2025-05-14 15:50:40 +0100

    feat(badge): add tier and stage color support to BadgeComponent
    Introduce `tierColor` and `stageColor` utility functions to handle color logic for different tiers and stages. Extend `BadgeComponent` to accept `tier` and `stage` props, allowing dynamic color assignment based on the provided status. This enhances the visual representation of badges in the eligibility center and onboarding pages.

--- src/ui/components/Badge/index.tsx
+++ src/ui/components/Badge/index.tsx
@@ -1,23 +1,45 @@
-import { activeBadgeColor, approvedBadgeColor } from "@/lib/utils";
+import {
+  activeBadgeColor,
+  approvedBadgeColor,
+  STAGE,
+  stageColor,
+  tierColor,
+} from "@/lib/utils";
 import { Badge, BadgeProps } from "@mantine/core";
 import { ReactNode } from "react";
 
 interface IBadge extends BadgeProps {
   active?: boolean;
+  tier?: boolean;
+  stage?: boolean;
   status: string;
 }
 
-export const BadgeComponent = ({ active, status, ...props }: IBadge) => {
+export const BadgeComponent = ({
+  active,
+  tier,
+  stage,
+  status,
+  ...props
+}: IBadge) => {
   return (
     <Badge
-      {...props}
       tt="capitalize"
       variant="light"
-      color={active ? activeBadgeColor(status) : approvedBadgeColor(status)}
+      color={
+        active
+          ? activeBadgeColor(status)
+          : tier
+          ? tierColor(status)
+          : stage
+          ? stageColor(status as STAGE)
+          : approvedBadgeColor(status)
+      }
       w={props.w ? props.w : 82}
       h={props.h ? props.h : 24}
       fw={props.fw ? props.fw : 400}
       fz={props.fz ? props.fz : 12}
+      {...props}
     >
       {status.toLowerCase()}
     </Badge>

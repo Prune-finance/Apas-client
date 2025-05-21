e50e2c9af76899caa4e046a53204c05e2996500d
Gilead Odo <63629666+blur94@users.noreply.github.com>
2025-05-21 15:33:50 +0100

    refactor(onboarding): update interfaces, add document approval logic, and improve form handling
    - Update `OnboardingState` interface to use `OnboardingBusiness` instead of `IOnboarding`
    - Add min and max date validation for directors and shareholders' date of birth
    - Enhance document approval logic with status badges and revalidation
    - Improve form handling for onboarding and eligibility center components

--- src/app/(onboarding)/onboarding/AddDirectorsInfo.tsx
+++ src/app/(onboarding)/onboarding/AddDirectorsInfo.tsx
@@ -105,6 +105,8 @@ export const AddDirectorsInfo = ({
             <DateInputWithInsideLabel
               label="Date of Birth"
               w="100%"
+              minDate={new Date("1900-01-01")}
+              maxDate={new Date()}
               key={form.key(`directors.${index}.date_of_birth`)}
               {...form.getInputProps(`directors.${index}.date_of_birth`)}
             />

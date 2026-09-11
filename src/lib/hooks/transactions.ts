605ee44973ede3ae3a58a9098d24e71369291ab1
Marcia Cole <marcia@Marcias-MacBook-Pro.local>
2026-09-11 12:43:52 +0100

    fix: update on new design

--- src/lib/hooks/transactions.ts
+++ src/lib/hooks/transactions.ts
@@ -1253,7 +1253,7 @@ export function exportSingleUserAccountTransactions(accountID: string, params: I
 
 export function exportAdminSingleAccountTransactions(accountID: string, params: IParams) {
   return openExportUrl(
-    axios.get(`accounts/business/company-account/${accountID}/transactions/export`, {
+    axios.get(`admin/accounts/business/company-account/${accountID}/transactions/export`, {
       params: sanitizedQueryParams(params),
     })
   );

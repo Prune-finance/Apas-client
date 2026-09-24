331617736c4a1fbc1eef3518af17f51f7a3c615c
ibraheem-kabir <ibraheem.kabir@meltwater.com>
2026-09-24 13:24:21 +0100

    handled issued account

--- src/ui/components/SingleAccount/(tabs)/Transactions.tsx
+++ src/ui/components/SingleAccount/(tabs)/Transactions.tsx
@@ -208,7 +208,7 @@ export const Transactions = ({
     const urlMap: { [key: string]: string } = {
       payout: `${baseUrl}/accounts/payout/${accountID}/transactions/statement?date=${startDate}&endDate=${endDate}&currencyCode=${currencyType}`,
       "admin-account": `${baseUrl}/admin/accounts/${accountID}/transactions/statement?date=${startDate}&endDate=${endDate}`,
-      "issued_account": `${baseUrl}/admin/accounts/${accountID}/transactions/statement?date=${startDate}&endDate=${endDate}`,
+      "issued_account": `${baseUrl}/accounts/${accountID}/statement?date=${startDate}&endDate=${endDate}&currencyCode=${currencyType}`,
       "admin-payout": `${baseUrl}/admin/accounts/payout/${accountID}/transactions/statement?date=${startDate}&endDate=${endDate}`,
       "admin-default": `${baseUrl}/admin/accounts/business/${accountID}/transactions/statement?date=${startDate}&endDate=${endDate}`,
       "own-account": `${baseUrl}/accounts/company/${accountID}/transactions/statement?date=${startDate}&endDate=${endDate}`,
